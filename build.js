96be7e9175dcf1ed8b7ac2c539ed91ef33ef7e11
yannick <yannick.connan@gmail.com>
2012-05-07 18:48:42 +0100

    created sublime text project file

--- build.js
+++ build.js
@@ -6,62 +6,78 @@ var fs = require('fs');
 
 
 var config = {
-    baseUrl: './lib',
-    name: 'extern/almond',
-    include: ["main"],
-    out: './build/aex.build.js',
-    optimize: "none",
-    inlineText: true,
-    wrap: true
+    baseUrl    : './lib',
+    name       : 'extern/almond',
+    include    : ["main"],
+    out        : './build/aex.build.js',
+    optimize   : "none",
+    inlineText : true,
+    wrap       : true
 };
+
+console.log("building optimized library");
 
 requirejs.optimize(config, function (buildResponse) {
 
-    var contents = fs.readFileSync(config.out, 'utf8');
-    var jsp = require("uglify-js").parser;
-    var pro = require("uglify-js").uglify;
-    var ast = jsp.parse(contents); // parse code and get the initial AST
+    console.log("Optimzed library built");
+    console.log("Minimizing optimized library");
+
+    var contents = fs.readFileSync(config.out, 'utf8'),
+        jsp      = require("uglify-js").parser,
+        pro      = require("uglify-js").uglify,
+        ast      = jsp.parse(contents); // parse code and get the initial AST
+
+
     ast = pro.ast_mangle(ast,{toplevel:true}); // get a new AST with mangled names
     ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
+
     var final_contents = pro.gen_code(ast); // compressed code here
     fs.writeFileSync('./build/aex.min.js', final_contents, 'utf8');
+
+    console.log("Optimized library minimized");
 });
 
 var exporter_config = {
 
-    baseUrl: './lib',
-    name: 'extern/almond',
-    include: ["exporter"],
-    out: './build/exporter/aex_export.jsx',
-    optimize: "none",
-    inlineText: true,
-    wrap: {
-        start: "var global = this; \n (function() {",
-        end: "})();"
+    baseUrl    : './lib',
+    name       : 'extern/almond',
+    include    : ["exporter"],
+    out        : './build/exporter/aex_export.jsx',
+    optimize   : "none",
+    inlineText : true,
+    wrap       : {
+
+        start  : "var global = this; \n (function() {",
+        end    : "})();"
     }
 };
 
+console.log("Building exporter");
+
 requirejs.optimize(exporter_config, function (buildResponse) {
 
-    var contents = fs.readFileSync(exporter_config.out, 'utf8');
-    var ae_cs55 = '/Applications/Adobe After Effects CS5.5/Scripts/ScriptUI Panels';
-    var ae_cs5 = '/Applications/Adobe After Effects CS5/Scripts/ScriptUI Panels';
+    console.log("Exporter built");
 
-    var new_jsx;
-    var old_jsx = fs.createReadStream('./build/exporter/aex_export.jsx');
-
-    var new_swf;
-    var old_swf = fs.createReadStream('./build/exporter/timeout.swf');
+    var contents = fs.readFileSync(exporter_config.out, 'utf8'),
+        ae_cs55  = '/Applications/Adobe After Effects CS5.5/Scripts/ScriptUI Panels',
+        ae_cs5   = '/Applications/Adobe After Effects CS5/Scripts/ScriptUI Panels',
+        old_jsx  = fs.createReadStream('./build/exporter/aex_export.jsx'),
+        old_swf  = fs.createReadStream('./build/exporter/timeout.swf'),
+        new_jsx, new_swf;
 
     try {
 
         if (fs.lstatSync(ae_cs55).isDirectory()) {
+
+            console.log("After Effects CS5.5 detected, installing exporter");
 
             new_jsx = fs.createWriteStream(ae_cs55+'/aex_export.jsx');
             require('util').pump(old_jsx, new_jsx);
 
             new_swf = fs.createWriteStream(ae_cs55+'/timeout.swf');
             require('util').pump(old_swf, new_swf);
+
+            console.log("Exporter installed on After Effects CS5.5");
         }
 
     } catch (e) {
@@ -71,11 +87,15 @@ requirejs.optimize(exporter_config, function (buildResponse) {
     try {
         if (fs.lstatSync(ae_cs5).isDirectory()) {
 
+            console.log("After Effects CS5 detected, installing exporter");
+
             new_jsx = fs.createWriteStream(ae_cs5+'/aex_export.jsx');
             require('util').pump(old_jsx, new_jsx);
 
             new_swf = fs.createWriteStream(ae_cs5+'/timeout.swf');
             require('util').pump(old_swf, new_swf);
+
+            console.log("Exporter installed on After Effects CS5");
         }
     } catch (e) {
 
