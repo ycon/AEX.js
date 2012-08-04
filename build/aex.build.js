b790e37b4c8283f7bda90be807daee3366338991
Yannick <yannick.connan@gmail.com>
2012-08-04 14:17:40 +0100

    docking around SVG again

--- build/aex.build.js
+++ build/aex.build.js
@@ -4828,6 +4828,22 @@ define('renderers/svg/svgUtils',[],function () {
 
     }
 
+    function setRect (node, rect) {
+        var seq_list = node.pathSegList,
+            i = 1,
+            move = 'createSVGPathSegMovetoAbs',
+            line = 'createSVGPathSegLinetoAbs';
+
+        seq_list.clear();
+
+        seq_list.appendItem(node[move](rect.x, rect.y));
+        seq_list.appendItem(node[line](rect.x + rect.width, rect.y));
+        seq_list.appendItem(node[line](rect.x + rect.width, rect.y + rect.height));
+        seq_list.appendItem(node[line](rect.x, rect.y + rect.height));
+        seq_list.appendItem(node[line](rect.x, rect.y));
+
+    }
+
     function setPath (node, path, invert) {
 
 
@@ -4838,7 +4854,7 @@ define('renderers/svg/svgUtils',[],function () {
             move = 'createSVGPathSegMovetoAbs',
             line = 'createSVGPathSegLinetoAbs',
             curve = 'createSVGPathSegCurvetoCubicAbs',
-            padding = (invert ? 5 : 0),
+            padding = 0,//(invert ? 5 : 0),
             patch, prev_anchor, item, anchor;
 
         if (!l) {
@@ -4847,9 +4863,9 @@ define('renderers/svg/svgUtils',[],function () {
 
         if (l + (path.closed ? 1 : 0) + padding !== seq_list.numberOfItems) {
 
-            console.log("remove");
             seq_list.clear();
 
+            /*
             if (invert) {
                 seq_list.appendItem(node[move](-big_number, -big_number));
                 seq_list.appendItem(node[line](big_number, -big_number));
@@ -4857,6 +4873,7 @@ define('renderers/svg/svgUtils',[],function () {
                 seq_list.appendItem(node[line](-big_number, big_number));
                 seq_list.appendItem(node[line](-big_number, -big_number));
             }
+            */
 
             seq_list.appendItem(node[move](0, 0));
 
@@ -4923,7 +4940,8 @@ define('renderers/svg/svgUtils',[],function () {
 
     return {
         create: create,
-        path: setPath
+        path: setPath,
+        rect: setRect
     };
 
 });
@@ -4950,49 +4968,133 @@ define('renderers/svg/svgMask',[
     var svg_mask_counter = 0;
 
 
-    function updateMask(node, mask) {
+    function updateMask(node, mask, is_first, scale, bounds, defs) {
 
-        utils.path(node.pathNode, mask, mask.inverted);
-        node.style.opacity = mask.opacity;
+        var is_first_sub = (mask.blending === 'subtract') && is_first,
+            to_invert = (is_first_sub) ? !mask.inverted : mask.inverted,
+            expansion = mask.expansion * (to_invert ? -1 : 1),
+            feather = mask.feather,
+            filter = node.filter,
+            path_node = node.path,
+            is_sub = (mask.blending === 'subtract' && !is_first),
+            fill_color = (to_invert)
+                            ? 'black'
+                            : (is_sub)
+                                ? 'black'
+                                : 'white',
+            inv_mask;
 
-        if (mask.expansion > 0) {
+        if (to_invert) {
+            if (!node.invMask) {
 
-            node.style.strokeWidth = (mask.expansion * 2) + 'px';
-        } else {
-            node.style.strokeWidth = 0;
+                inv_mask = node.invMask = defs.create('mask', {
+                    id: path_node.id + "_mask"
+                });
+
+                node.invMaskBG = inv_mask.create('path', {}, {
+
+                    fill: 'white',
+                    strokeLinejoin: 'round',
+                    stroke: 'white',
+                    strokeWidth: '0px',
+                    fillRulle: 'evenodd'
+
+                });
+
+                node.invMaskPath = inv_mask.create('path', {}, {
+
+                    fill: 'black',
+                    strokeLinejoin: 'round',
+                    stroke: 'black',
+                    strokeWidth: '0px',
+                    fillRulle: 'evenodd'
+
+                });
+
+
+                node.style.mask = 'url(#' + path_node.id + '_mask)';
+            }
+            path_node = node.invMaskPath;
+
+            node.style.fill = is_sub ? 'white' : 'black';
+            utils.rect(node.path, bounds);
+            utils.rect(node.invMaskBG, bounds);
+
+        } else if (node.invMask) {
+            node.style.mask = '';
+            //TODO: Remove mask when de remove inversion
         }
+
+        utils.path(path_node, mask, to_invert);
+
+        node.style.opacity = mask.opacity;
+        path_node.style.fill = fill_color;
+
+        if (expansion !== 0) {
+            path_node.style.strokeWidth = Math.abs(expansion * 2) + 'px';
+            path_node.style.stroke = (expansion > 0 !== (fill_color === 'white')) ? 'white' : 'black';
+        } else {
+            path_node.style.strokeWidth = 0;
+        }
+
+        if (feather.x || feather.y) {
+
+            path_node.style.filter = 'url(#' + filter.id + ')';
+
+            filter.blur.stdDeviationX.baseVal = feather.x / 2;
+            filter.blur.stdDeviationY.baseVal = feather.y / 2;
+            //filter.x.baseVal.newValueSpecifiedUnits(5, -300);
+        } else {
+            path_node.style.filter = '';
+        }
+
     }
 
     function renderMask(root, masks, scale, bounds) {
 
         var mask_node  = root.maskNode,
+            defs       = root.defs,
             children   = mask_node.childNodes,
             l          = masks.getLength(),
             i          = children.length,
             to_add     = i < l,
-            big_number = 90000,
-            group;
+            is_first = true,
+            mask_layer, path, filter, blend;
 
 
         if (i !== l) {
             if (to_add) {
                 for (; i < l; i += 1) {
-                    group = mask_node.create('g', {
-                    }, {
-                        'fill': 'white',
-                        'stroke-linejoin': 'round',
-                        'stroke': 'white',
-                        'strokeWidth': '0px',
-                        'fillRulle': 'evenodd'
-                    });
-                    group.pathNode = group.create('path', {
+                    mask_layer = mask_node.create('g');
+                    mask_layer.path = mask_layer.create('path', {
                         id: mask_node.id + '_' + i + '_shape'
+                    }, {
+                        fill: 'white',
+                        strokeLinejoin: 'round',
+                        stroke: 'white',
+                        strokeWidth: '0px',
+                        fillRulle: 'evenodd'
+
                     });
+                    filter = defs.create('filter', {
+                        id: mask_node.id + '_' + i + '_filter'
+                    });
+                    filter.blur = filter.create('feGaussianBlur', {
+                        'in': 'SourceGraphic'
+                    });
+                    mask_layer.filter = filter;
+
+                    blend = defs.create('filter', {
+                        id: mask_node.id + '_' + i + '_blend'
+                    });
+
+                    filter.blend = blend.create();
+
 
                 }
             } else {
                 for (; i >= l; i -= 1) {
-                    //remove mask
+                    //TODO: Remove mask
                 }
             }
 
@@ -5003,7 +5105,8 @@ define('renderers/svg/svgMask',[
         i = 0;
 
         for (; i < l; i += 1) {
-            updateMask(children[i], masks.get(i));
+            updateMask(children[i], masks.get(i), is_first, scale, bounds, defs);
+            is_first = false;
         }
 
 
@@ -5013,10 +5116,12 @@ define('renderers/svg/svgMask',[
 
         var mask_name = 'svg_mask_' + (svg_mask_counter += 1),
             svg_node = utils.create('svg', {height: 0}),
+            svg_defs = svg_node.create('defs'),
             svg_mask = svg_node.create('mask', {
                 id: mask_name
             });
 
+        svg_node.defs = svg_defs;
         svg_node.maskNode = svg_mask;
         element.appendChild(svg_node);
         element.firstChild.style.mask = 'url(#' + mask_name + ")";
